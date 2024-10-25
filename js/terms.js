8787361309dab6df7d7bd67a8b7adc8d6c111a5a
koinonia2 <koinonia_ys@naver.com>
2024-10-25 17:50:53 +0900

    [update&add] terms 수정 / jquery 추가

--- js/terms.js
+++ js/terms.js
@@ -2,6 +2,10 @@ window.addEventListener("load", function() {
     const checkbox = document.querySelector("#check_box");
     const checkboxIcon = document.querySelector(".check_box_icon > i");
     const nextBtn = document.querySelector(".next_btn > button");
+    function termsSession(boolean) {
+        sessionStorage.setItem("termsAgree", boolean);
+    };
+    
     checkbox.addEventListener("change", function() {
         if(checkbox.checked) {
             checkboxIcon.style.color = "#9FA9D8";
@@ -13,4 +17,12 @@ window.addEventListener("load", function() {
             nextBtn.disabled = true;
         };
     });
+    nextBtn.addEventListener("click", function() {
+        termsSession("true");
+        window.location.href = "index.html";
+    });
 });
+// 뒤로 가기 버튼 눌렀을 때 세션 스토리지 초기화
+window.onpopstate = function() {
+    sessionStorage.removeItem("termsAgree");
+};
